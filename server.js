07b91d2823dfc0b105402a7026e5881755b3b181
ringhietto <santa.network02@gmail.com>
2024-12-17 12:58:42 +0100

    San Pietro Forino

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
-const express = require('express');
-const path = require('path');
-const { SerialPort } = require('serialport');
-const WebSocket = require('ws');
+import { WebSocketServer } from 'ws';
+import { SerialPort, ReadlineParser } from 'serialport';
 
-const app = express();
-const port = 3000;
+const WS_PORT = 8001;
+const SERIAL_PORT = '/dev/cu.usbmodem1101';
+const BAUD_RATE = 9600;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const wss = new WebSocketServer({ port: WS_PORT });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+wss.on('connection', function connection(ws) {
+  console.log("WebSocket client connected");
+  ws.on('error', console.error);
+
+  ws.send('connected');
 });
 
-// Inizializza WebSocket per la comunicazione tra browser e server
-const server = app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+const port = new SerialPort({
+  path: SERIAL_PORT,
+  baudRate: BAUD_RATE,
+}); 
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+parser.on('data', function (data) {
+  console.log(data);
+  
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === client.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
 });
 
-const wss = new WebSocket.Server({ server });
-
-// Configura la porta seriale
-const serial = new SerialPort({
-    path: '/dev/tty.usbmodem101', // Aggiorna il percorso con il corretto
-    baudRate: 9600,
-});
-
-serial.on('open', () => {
-    console.log('Connessione seriale aperta.');
-});
-
-serial.on('data', (data) => {
-    console.log('Dati ricevuti da Arduino:', data.toString());
-    // Invia i dati al client tramite WebSocket
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data.toString());
-        }
-    });
+port.on('error', function (err) {
+  console.error('Serial Port Error:', err.message);
 });
